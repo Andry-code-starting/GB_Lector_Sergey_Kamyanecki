43b586a8eb0f5b447ebf15dc2b926f27667c8efb
Andry <lightk550i@mail.ru>
2022-07-26 22:03:06 +0500

    Интересное в 006 ищем большее значение а в 005 сравнеение имени и соответственно выводитьсяпод Андрей своё сообщение

---
+++ HelloWorld/Example005_HelloConsoile/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+String username = Console.ReadLine();
+
+if(username.ToLower == "андрей");
+{
+    Console.WriteLine("Ура, это же Андрей!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
---
+++ HelloWorld/Exemple003_HelloConsoile/Program.cs
@@ -0,0 +1,4 @@
+﻿int numberA = 7;
+int numderB = 5;
+int result = numberA + numderB;
+Console.WriteLine (result);
